d47919dadb77e024de23c1f425c0b93b4f7e44b9
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-07 13:56:21 +0000

    ⚙️ Invite Command Update v1.1

--- src/commands/invite.js
+++ src/commands/invite.js
@@ -5,8 +5,6 @@ module.exports.run = async (bot, message, args) => {
         .setTitle(bot.user.tag)
         .setDescription(`Hydra+ is a fun economy bot with various Items and is easy to use. 
     [Invite](https://discord.com/oauth2/authorize?client_id=679710920334639115&scope=bot&permissions=19456) Hydra+ to your server today!\nYou can also hangout in our [Discord Server](https://discord.gg/XqezQaK)`)
-        //.setFooter(`Tip: you can vote our bot and gain 4,000 coins for free.`)
-        //.setURL(`https://top.gg/bot/752061324682461255`)
         .setThumbnail(bot.user.displayAvatarURL({ format: 'png', size: 256, dynamic: true }))
         .setColor('#81bcfc')
     message.channel.send(InviteEmbed)
