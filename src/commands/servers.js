211e241feea4251104e0002961840d5db69b3da5
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-10 19:22:34 +0000

    Update servers.js

--- src/commands/servers.js
+++ src/commands/servers.js
@@ -1,5 +1,5 @@
 module.exports.run = async (bot, message, args) => {
-    message.channel.send(`Servers amount: **${bot.guilds.cache.size.toLocaleString()}**\nUsers amount: **${bot.users.cache.size}**`);;
+    message.channel.send(`Servers amount: **${bot.guilds.cache.size.toLocaleString()}**`);;
 }
 module.exports.config = {
     name: 'servers', // Command Name
