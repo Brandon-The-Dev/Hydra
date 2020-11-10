aac1bb2478091ff0b9873c6302bb06fc1d00edf4
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-10 19:21:09 +0000

    Update beg.js

--- src/commands/beg.js
+++ src/commands/beg.js
@@ -27,6 +27,6 @@ module.exports.config = {
     userPerms: [], // User permissions needed to run command. Leave empty if nothing.
     aliases: [], // Aliases 
     bankSpace: 15, // Amount of bank space to give when command is used.
-    cooldown: 30 // Command Cooldown
+    cooldown: 10 // Command Cooldown
 }
 
