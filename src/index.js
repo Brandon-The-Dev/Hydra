8528a76ff70e21a663cc0b22d32d3249553ba694
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-16 10:04:48 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 const { Collection } = require('discord.js');
+const DBL = require("dblapi.js");
 const MongoClient = require('./utils/MongoClient');
 const bot = new MongoClient({ ws: { intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS'] }, fetchAllMembers: false });
+const dbl = new DBL(process.env.TOPGGWEBHOOK, bot);
 
 bot.login(process.env.TOKEN);
 
 bot.commands = new Collection();
 bot.aliases = new Collection();
 bot.cooldowns = new Collection();
+bot.dbl = dbl;
 
 require('./utils/handlers/command')(bot);
 require('./utils/handlers/event')(bot);
