f25fba087da5d7eeee7b5c595b10af6487db7b93
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-17 15:48:25 +0000

    Update and rename weekly.js to vote.js

---
+++ src/commands/vote.js
@@ -0,0 +1,14 @@
+module.exports.run = async (bot, message, args) => {
+message.channel.send(`Vote For Hydra+ Here : \n https://top.gg/bot/679710920334639115/vote`);
+}
+
+module.exports.config = {
+    name: 'vote', // Command Name
+    description: 'allows you to vote.', // Description
+    usage: 'h vote', // Usage
+    botPerms: [], // Bot permissions needed to run command. Leave empty if nothing.
+    userPerms: [], // User permissions needed to run command. Leave empty if nothing.
+    aliases: [], // Aliases 
+    bankSpace: 15, // Amount of bank space to give when command is used.
+    cooldown: 10 // Command Cooldown
+}
