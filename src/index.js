701673c140654435b8887fb57d4cf33eef82a527
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-17 15:59:24 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const { Collection } = require('discord.js');
 const DBL = require("dblapi.js");
 const MongoClient = require('./utils/MongoClient');
 const bot = new MongoClient({ ws: { intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS'] }, fetchAllMembers: false });
-const dbl = new DBL(process.env.TOPGGWEBHOOK, bot);
+const dbl = new DBL(process.env.TOPGGWEBHOOK, bot, { webhookPort: 5002, webhookAuth: '${{secrets.AUTH}}' }););
 
 bot.login(process.env.TOKEN);
 
@@ -15,10 +15,9 @@ bot.dbl = dbl;
 require('./utils/handlers/command')(bot);
 require('./utils/handlers/event')(bot);
 
-/*dbl.webhook.on('vote', async (vote) => {
+dbl.webhook.on('vote', async (vote) => {
     const user = bot.users.cache.fetch(vote.user);
     if(user){
     hook.send(`${user.username} just voted ${bot.user.username}`);
             }
 })
-*/
