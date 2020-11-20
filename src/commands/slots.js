125345e1aa2c8c190d11238251b1d2e0e08ea9c0
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-20 13:58:26 +0000

    Update slots.js
    fixed the lost embed and args

--- src/commands/slots.js
+++ src/commands/slots.js
@@ -23,8 +23,10 @@ module.exports.run = async (bot, message, args) => {
     .setColor("RED")
     .setDescription(`❌ The minimum you can gamble is \`50\` coins.`);
 
-           if (betAmount < 50) return message.channel.send(coinmin);
-           if (betAmount == 'all' || betAmount == 'max') betAmount=userData.coinsInWallet;
+    if (betAmount < 50) return message.channel.send(coinmin);
+  
+    if (betAmount == 'all' || betAmount == 'max') betAmount=userData.coinsInWallet;
+    else betAmount=parseInt(args[0]);
 
     let moneywarn = new MessageEmbed()
     .setColor("RED")
@@ -55,7 +57,7 @@ module.exports.run = async (bot, message, args) => {
         betAmount *= 1.3
         win = true;
     } else if (number[0] == number[1] || number[0] == number[2] || number[1] == number[2]) { 
-        betAmount *= 1.6
+        betAmount *= 1.5
         win = true;
     }
     if (win) {
@@ -71,7 +73,7 @@ module.exports.run = async (bot, message, args) => {
         userData.coinsInWallet -= parseInt(betAmount);
         await userData.save();
         let slotsEmbed = new MessageEmbed()
-            .setDescription(`**Slots V2**\n\n${slotItems[number[0]]} | ${slotItems[number[1]]} | ${slotItems[number[2]]}\n\nYou lost ${betAmount} coins`)
+            .setDescription(`**Slots V2**\n\n${slotItems[number[0]]} | ${slotItems[number[1]]} | ${slotItems[number[2]]}\n\nYou lost ${lostCoins.toLocaleString()} coins`)
             .setColor("RED")
             .setThumbnail(member.user.displayAvatarURL({ format: 'png', size: 256, dynamic: true }))
             .setFooter("https://top.gg/bot/679710920334639115/vote")
@@ -84,21 +86,16 @@ module.exports.run = async (bot, message, args) => {
     let coinsInWallet = await bot.fetchUser(message.author.id);
     let money = parseInt(args[0]);
     let win = false;
-
     let moneymore = new MessageEmbed()
     .setColor("GREEN")
     .setDescription(`❌ You are betting more than you have`);
-
     let moneyhelp = new MessageEmbed()
     .setColor("GREEN")
     .setDescription(`❌ Specify an amount`); 
-
     if (!money) return message.channel.send(moneyhelp);
     if (money > moneydb) return message.channel.send(moneymore);
-
     let number = []
     for (let i = 0; i < 3; i++) { number[i] = Math.floor(Math.random() * slotItems.length); }
-
     if (number[0] == number[1] && number[1] == number[2])  { 
         money *= 9
         win = true;
@@ -121,7 +118,6 @@ module.exports.run = async (bot, message, args) => {
         userData.coinsInWallet -= parseInt(betAmount);
         await userData.save();
     }
-
 }
 */
 
