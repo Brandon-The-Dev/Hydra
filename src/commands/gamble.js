a81c65bd836b5361976e1fcea839b743b5f64bf2
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-21 15:37:52 +0000

    Update gamble.js

--- src/commands/gamble.js
+++ src/commands/gamble.js
@@ -156,5 +156,5 @@ module.exports.config = {
     userPerms: [], // User permissions needed to run command. Leave empty if nothing.
     aliases: [], // Aliases 
     bankSpace: 5, // Amount of bank space to give when command is used.
-    cooldown: 10 // Command Cooldown
+    cooldown: 5 // Command Cooldown
 }
