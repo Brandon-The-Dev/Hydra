c4e4c94c6065f5ee4cda483075339f72557289b4
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-21 15:29:15 +0000

    Update dice.js

--- src/commands/dice.js
+++ src/commands/dice.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require("discord.js");
+/*const { MessageEmbed } = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     const botRoll = Math.floor(Math.random() * 7)+1;
     const userChoice = Math.floor(Math.random() * 7)+1;
@@ -61,6 +61,75 @@ module.exports.run = async (bot, message, args) => {
         .setColor('RED')
         .setDescription(`Bot rolled: \`${botRoll}\`\nYou rolled: \`${userChoice}\`\nYou lost: \`${lostCoins.toLocaleString()}\` coins`)
         .setTitle('You lost!')
+        message.channel.send(lostEmbed);
+    }
+}   */
+const { MessageEmbed } = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+    const botRoll = Math.floor(Math.random() * 7)+1;
+    const userChoice = Math.floor(Math.random() * 7)+1;
+    const userData = await bot.fetchUser(message.author.id);
+    const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+    let passivewarn = new MessageEmbed()
+    .setColor("RED")
+    .setDescription(`❌ You have \`PASSIVE\` enabled, your reqired to disable it to use this command.`);
+  
+    if (userData.passive == true) return message.channel.send(passivewarn);
+  
+    let betAmount = args[0];
+    const result = userChoice-botRoll;
+  
+    let coinswarn = new MessageEmbed()
+    .setColor("RED")
+    .setDescription(`❌ Enter the amount you want to gamble. `);
+
+    if (!betAmount || isNaN(betAmount) && betAmount !== 'all' && betAmount !== 'max') return message.channel.send(coinswarn);
+
+    let coinmin = new MessageEmbed()
+    .setColor("RED")
+    .setDescription(`❌ The minimum you can gamble is \`200\` coins.`);
+
+    if (betAmount < 200) return message.channel.send(coinmin);
+
+    if (betAmount == 'all' || betAmount == 'max') betAmount=userData.coinsInWallet;
+    else betAmount=parseInt(args[0]);
+  
+    let moneywarn = new MessageEmbed()
+    .setColor("RED")
+    .setDescription(`❌ You dont have that many coins.`);
+
+           if (betAmount > userData.coinsInWallet) {
+           return message.channel.send(moneywarn);
+           }
+  
+    if (botRoll < userChoice) {
+        const wonCoins = (betAmount + (betAmount * 0.55));
+        userData.coinsInWallet += parseInt(wonCoins);
+        await userData.save();
+        const wonEmbed = new MessageEmbed()
+        .setColor('GREEN')
+        .setThumbnail(member.user.displayAvatarURL({ format: 'png', size: 256, dynamic: true }))
+        .setFooter("https://top.gg/bot/679710920334639115/vote")
+        .setDescription(`Dice V2 | Player **${member.user.username}** \n\nHydra+ rolled: \`${botRoll}\` \n${member.user.username} rolled: \`${userChoice}\`\n\nWinnings: **${wonCoins.toLocaleString()}** coins`)
+        message.channel.send(wonEmbed);
+    } else if (botRoll == userChoice) {
+        userData.coinsInWallet -= betAmount/2
+        userData.save();
+        const tieEmbed = new MessageEmbed()
+        .setColor('YELLOW')
+        .setThumbnail(member.user.displayAvatarURL({ format: 'png', size: 256, dynamic: true }))
+        .setFooter("https://top.gg/bot/679710920334639115/vote")
+        .setDescription(`Dice V2 | Player **${member.user.username}** \n\nHydra+ rolled: \`${botRoll}\` \n${member.user.username} rolled: \`${userChoice}\`\n\n**${member.user.username}** & **Hydra+**: Tied\n\nLost: **${(betAmount/2).toLocaleString()}** coins`)
+        message.channel.send(tieEmbed);
+    } else if (botRoll > userChoice) {
+        const lostCoins = (betAmount);
+        userData.coinsInWallet -= parseInt(betAmount);
+        await userData.save();
+        const lostEmbed = new MessageEmbed()
+        .setColor('RED')
+        .setThumbnail(member.user.displayAvatarURL({ format: 'png', size: 256, dynamic: true }))
+        .setFooter("https://top.gg/bot/679710920334639115/vote")
+        .setDescription(`Dice V2 | Player **${member.user.username}** \n\nHydra+ rolled: \`${botRoll}\` \n${member.user.username} rolled: \`${userChoice}\`\n\nLost: **${lostCoins.toLocaleString()}** coins`)
         message.channel.send(lostEmbed);
     }
 }   
