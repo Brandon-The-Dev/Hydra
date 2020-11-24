56796387a9e2d0bc3951dc37c76c4e0650d67dd2
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-24 19:21:44 +0000

    🔥 Hydra+ System Update v2.0

--- src/commands/work.js
+++ src/commands/work.js
@@ -1,5 +1,9 @@
 const ms = require("parse-ms");
-const Discord = require("discord.js")
+const { MessageEmbed } = require("discord.js")
+const i = '<:info:688057843558908013>'
+const x = '<:noov:695993429087354991> '
+const tick = '<:bigtick:779736050892931082>'
+
 module.exports.run = async (bot, message, args) => {
 
     let user = message.author;
@@ -10,19 +14,18 @@ module.exports.run = async (bot, message, args) => {
     if (author !== null && timeout - (Date.now() - author) > 0) {
         let time = ms(timeout - (Date.now() - author));
     
-        let timeEmbed = new Discord.MessageEmbed()
-        .setColor("#FFFFFF")
-        .setDescription(`<:Cross:618736602901905418> You have already worked recently\n\nTry again in ${time.minutes}m ${time.seconds}s `);
+        let timeEmbed = new MessageEmbed()
+        .setColor("BLUE")
+        .setDescription(`${i} **${user.user.username}** : You have already worked recently\n\nTry again in ${time.minutes}m ${time.seconds}s `);
         message.channel.send(timeEmbed)
       } else {
-
         let replies = ['Programmer','Builder','Waiter','Busboy','Chief','Mechanic']
 
         let result = Math.floor((Math.random() * replies.length));
         let amount = Math.floor(Math.random() * 300) + 1;
-        let embed1 = new Discord.MessageEmbed()
-        .setColor("#FFFFFF")
-        .setDescription(`✅ You worked as a ${replies[result]} and earned ${amount} coins`);
+        let embed1 = new MessageEmbed()
+        .setColor("BLUE")
+        .setDescription(`${i} You worked as a ${replies[result]} and earned ${amount} coins`);
         message.channel.send(embed1)
         
         bot.giveCoins(message.author.id, amount)
@@ -36,7 +39,7 @@ module.exports.config = {
     botPerms: [], // Bot permissions needed to run command. Leave empty if nothing.
     userPerms: [], // User permissions needed to run command. Leave empty if nothing.
     aliases: [], // Aliases 
-    bankSpace: 0, // Amount of bank space to give when command is used.
-    cooldown: 30 // Command Cooldown
+    bankSpace: 50, // Amount of bank space to give when command is used.
+    cooldown: 15 // Command Cooldown
 }
 
