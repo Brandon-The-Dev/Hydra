083bd3a70ac8893c857013e8899d7326c7046275
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-24 18:40:19 +0000

    🔥 Hydra+ System Update v2.0

--- src/commands/buy.js
+++ src/commands/buy.js
@@ -1,23 +1,48 @@
+const { MessageEmbed } = require('discord.js');
 const itemss = require('../utils/items');
+const i = '<:info:688057843558908013>'
+const x = '<:noov:695993429087354991> '
+const tick = '<:yees:695992617015574534>'
 
 module.exports.run = async (bot, message, args) => {
+  const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
     let user = await bot.fetchUser(message.author.id);
     if (!args.join(' ')) {
-        return message.channel.send("you can't buy nothing, please enter the correct item id");
+            let buynothingerrorembed = new MessageEmbed()
+            .setColor("RED")
+            .setDescription(`${x} **${member.user.username}** : You can't buy nothing, please enter the correct item \`id\`.`);
+
+            return message.channel.send(buynothingerrorembed).catch();
+        //return message.channel.send("you can't buy nothing, please enter the correct item id");
     }
     if (!args[1]) args[1] = '';
     const item = itemss.find(x => x.name.toLowerCase() === args.join(' ').toString().toLowerCase() || x.name.toLowerCase() === args[0].toString().toLowerCase() || x.name.toLowerCase() === `${args[0].toString().toLowerCase()} ${args[1].toString().toLowerCase()}`);
     if (!item) {
-        return message.channel.send("You can't buy an item that doesn't exist please use the correct item id");
+            let wrongiderrorembed = new MessageEmbed()
+            .setColor("RED")
+            .setDescription(`${x} **${member.user.username}** : You can't buy an item that doesn't exist please use the correct item \`id\`.`);
+
+            return message.channel.send(wrongiderrorembed).catch();
+        //return message.channel.send("You can't buy an item that doesn't exist please use the correct item id");
     }
     if (item.canBuy == false) {
-        return message.channel.send(":thinking: You can't buy this item");
+            let cantbuyerrorembed = new MessageEmbed()
+            .setColor("RED")
+            .setDescription(`${x} **${member.user.username}** : You can't buy this item.`);
+
+            return message.channel.send(cantbuyerrorembed).catch();
+        //return message.channel.send(":thinking: You can't buy this item");
     }
     let buyAmount = args.join(' ').toString().match(/([1-9][0-9]*)/)
     if (!buyAmount) buyAmount=1;
     else buyAmount = buyAmount[0]
     if (item.price > user.coinsInWallet || (buyAmount*item.price) > user.coinsInWallet) {
-        return message.channel.send("You dont have the funds to buy this item.");
+            let nomoneyerrorembed = new MessageEmbed()
+            .setColor("RED")
+            .setDescription(`${x} **${member.user.username}** : You dont have the funds to buy this item.`);
+
+            return message.channel.send(nomoneyerrorembed).catch();
+        //return message.channel.send("You dont have the funds to buy this item.");
     }
     let founditem = user.items.find(x => x.name.toLowerCase() === item.name.toLowerCase());
     let array = [];
@@ -41,7 +66,12 @@ module.exports.run = async (bot, message, args) => {
     }
     user.coinsInWallet -= (parseInt(item.price)*parseInt(buyAmount));
     await user.save();
-    message.channel.send(`You bought **${parseInt(buyAmount).toLocaleString()}** \`${item.name}\``);
+            let itempayedembed = new MessageEmbed()
+            .setColor("GREEN")
+            .setDescription(`${tick} **${member.user.username}** : You bought **${parseInt(buyAmount).toLocaleString()}** \`${item.name}\`.`);
+
+            message.channel.send(itempayedembed).catch();
+    //message.channel.send(`You bought **${parseInt(buyAmount).toLocaleString()}** \`${item.name}\``);
 }
 
 module.exports.config = {
@@ -52,5 +82,5 @@ module.exports.config = {
     userPerms: [], // User permissions needed to run command. Leave empty if nothing.
     aliases: [], // Aliases 
     bankSpace: 0, // Amount of bank space to give when command is used.
-    cooldown: 4 // Command Cooldown
+    cooldown: 5 // Command Cooldown
 }
