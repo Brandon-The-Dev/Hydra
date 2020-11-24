0ba4bfbd73cd70235febc5f8ca8808eec183874d
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-24 18:57:09 +0000

    🔥 Hydra+ System Update v2.0

--- src/commands/invite.js
+++ src/commands/invite.js
@@ -3,10 +3,11 @@ module.exports.run = async (bot, message, args) => {
     const InviteEmbed = new MessageEmbed()
 
         .setTitle(bot.user.tag)
+        .setFooter("https://top.gg/bot/679710920334639115/vote")
         .setDescription(`Hydra+ is a fun economy bot with various Items and is easy to use. 
     [Invite](https://discord.com/oauth2/authorize?client_id=679710920334639115&scope=bot&permissions=19456) Hydra+ to your server today!\nYou can also hangout in our [Discord Server](https://discord.gg/XqezQaK)`)
         .setThumbnail(bot.user.displayAvatarURL({ format: 'png', size: 256, dynamic: true }))
-        .setColor('#81bcfc')
+        .setColor('BLUE')
     message.channel.send(InviteEmbed)
 }
 
@@ -17,6 +18,6 @@ module.exports.config = {
     botPerms: [], // Bot permissions needed to run command. Leave empty if nothing.
     userPerms: [], // User permissions needed to run command. Leave empty if nothing.
     aliases: [], // Aliases 
-    bankSpace: 5, // Amount of bank space to give when command is used.
-    cooldown: 3 // Command Cooldown
+    bankSpace: 2, // Amount of bank space to give when command is used.
+    cooldown: 5 // Command Cooldown
 }
