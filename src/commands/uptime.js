536e62adeab865332d63a4cd33ae747cad0b2c87
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-24 19:16:25 +0000

    🔥 Hydra+ System Update v2.0

---
+++ src/commands/uptime.js
@@ -0,0 +1,20 @@
+const { Client, Message, MessageEmbed } = require("discord.js")
+const pm = require('pretty-ms');
+const i = '<:info:688057843558908013>'
+
+module.exports.run = async (bot, message, args) => {
+    const Embed = new MessageEmbed()
+    .setDescription(`${i} Uptime : ${pm(bot.uptime)}`)
+    .setColor("BLUE")
+    message.channel.send(Embed);
+}
+module.exports.config = {
+    name: 'uptime',
+    description: 'See the bot\'s uptime',
+    usage: 'h uptime',
+    botPerms: [],
+    userPerms: [],
+    aliases: ['servers'],
+    bankSpace: 1,
+    cooldown: 5
+}
