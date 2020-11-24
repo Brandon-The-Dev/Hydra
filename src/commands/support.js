7155281a7c6dbb2efbc2a399231256234aaf2ed4
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-24 19:13:42 +0000

    🔥 Hydra+ System Update v2.0

---
+++ src/commands/support.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require('discord.js')
+const i = '<:info:688057843558908013>'
+const x = '<:noov:695993429087354991> '
+const tick = '<:bigtick:779736050892931082>'
+module.exports.run = async (bot, message, args) => {
+      const member = message.member;
+      const supportEmbed = new MessageEmbed()
+
+        .setDescription(`${i} **${member.user.username}** : If you have came across an \`ERROR\` or genuinely need a little help with the bot please feel free to join the server and create a ticket and the support team or the developers will respond as soon as they are available. [Support Server](https://discord.gg/XqezQaK)`)
+        .setColor('#81bcfc')
+    message.channel.send(supportEmbed)
+}
+module.exports.config = {
+    name: 'support', // Command Name
+    description: 'support server command.', // Description
+    usage: 'h support', // Usage
+    botPerms: [], // Bot permissions needed to run command. Leave empty if nothing.
+    userPerms: [], // User permissions needed to run command. Leave empty if nothing.
+    aliases: [], // Aliases 
+    bankSpace: 13, // Amount of bank space to give when command is used.
+    cooldown: 5 // Command Cooldown
+}
