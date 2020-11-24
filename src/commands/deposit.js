7193755fa64cda70591549612f994643f897794b
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-24 18:48:59 +0000

    🔥 Hydra+ System Update v2.0

--- src/commands/deposit.js
+++ src/commands/deposit.js
@@ -1,13 +1,39 @@
+const { MessageEmbed } = require('discord.js');
+const i = '<:info:688057843558908013>'
+const x = '<:noov:695993429087354991> '
+const tick = '<:yees:695992617015574534>'
 module.exports.run = async (bot, message, args) => {
     let data = await bot.fetchUser(message.author.id);
+  const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
 
     if (args.join(' ') === 'all') {
         if (data.coinsInWallet > data.bankSpace) {
             const max_deposit = (data.coinsInWallet + data.coinsInBank - data.bankSpace);
+          
+          if (data.coinsInBank-data.bankSpace === 0) {
+            let bankerrorembed = new MessageEmbed()
+            .setColor("RED")
+            .setDescription(`${x} **${member.user.username}** : Your bank is full.`);
 
+            return message.channel.send(bankerrorembed).catch();
+            //return message.channel.send("You don't have that much space in your bank. ");
+        }
+          
+         /* let test21 = new MessageEmbed()
+            .setColor("BLUE")
+            .setDescription(`${i} bank full`);
+          if (data.coinsInBank === data.bankSpace) return message.channel.send(test21);
+            //return message.channel.send('Your bank is not big enough.');
+          */
+          
+          
             data.coinsInWallet = max_deposit;
+            let dep111embed = new MessageEmbed()
+            .setColor("BLUE")
+            .setDescription(`${i} **${member.user.username}** : Deposited **${(data.bankSpace - data.coinsInBank).toLocaleString()}** coins.`);
 
-            await message.channel.send(`Deposited **${data.bankSpace - data.coinsInBank}** coins.`);
+            await message.channel.send(dep111embed).catch();
+            //await message.channel.send(`Deposited **${data.bankSpace - data.coinsInBank}** coins.`);
 
             data.coinsInBank = ((data.coinsInWallet + data.bankSpace) - max_deposit);
 
@@ -17,14 +43,24 @@ module.exports.run = async (bot, message, args) => {
             if ((data.coinsInWallet + data.coinsInBank) > data.bankSpace) {
                 const left = (data.coinsInWallet + data.coinsInBank) - data.bankSpace;
 
-                message.channel.send(`Deposited **${(data.coinsInWallet - left).toLocaleString()}** coins`);
+            let begembed = new MessageEmbed()
+            .setColor("BLUE")
+            .setDescription(`${i} **${member.user.username}** : Deposited **${(data.coinsInWallet - left).toLocaleString()}** coins`);
+
+            await message.channel.send(begembed).catch();
+              
+                //message.channel.send(`Deposited **${(data.coinsInWallet - left).toLocaleString()}** coins`);
                 
                 data.coinsInBank += (data.coinsInWallet - left);
                 data.coinsInWallet = left;
 
                 await data.save();
             } else {
-                message.channel.send(`Deposited **${(data.coinsInWallet).toLocaleString()}** coins`);
+            let dep111embed = new MessageEmbed()
+            .setColor("BLUE")
+            .setDescription(`${i} **${member.user.username}** : Deposited **${(data.coinsInWallet).toLocaleString()}** coins`);
+            await message.channel.send(dep111embed).catch();
+            //message.channel.send(`Deposited **${(data.coinsInWallet).toLocaleString()}** coins`);
 
                 data.coinsInBank += data.coinsInWallet;
                 data.coinsInWallet = 0;
@@ -33,25 +69,51 @@ module.exports.run = async (bot, message, args) => {
             }
         }
     } else {
-        if (isNaN(args[0])) {
-            return message.channel.send('That\'s not a number.');
+        let depAmount = parseInt(args[0]);
+        if (isNaN(depAmount)) {
+          let numbererrorembed = new MessageEmbed()
+            .setColor("RED")
+            .setDescription(`${x} **${member.user.username}** : That's not a number.`);
+
+            return message.channel.send(numbererrorembed).catch();
+            //return message.channel.send('That\'s not a number.');
         }
 
-        if (parseInt(args[0]) > data.bankSpace) {
-            return message.channel.send('Your bank is not big enough.');
+        if (parseInt(depAmount) > data.bankSpace) {
+            let bankfullerrorembed = new MessageEmbed()
+            .setColor("RED")
+            .setDescription(`${x} **${member.user.username}** : Your bank is not big enough.`);
+
+            return message.channel.send(bankfullerrorembed).catch();
+            //return message.channel.send('Your bank is not big enough.');
         }
-        if (parseInt(args[0]) > data.coinsInWallet) {
-            return message.channel.send("You don't have that much money.");
+        if (parseInt(depAmount) > data.coinsInWallet) {
+          let moneyerrorembed = new MessageEmbed()
+            .setColor("RED")
+            .setDescription(`${x} **${member.user.username}** : You don't have that much money.`);
+
+            return message.channel.send(moneyerrorembed).catch();
+            //return message.channel.send("You don't have that much money.");
         }
-        if (parseInt(args[0]) + data.coinsInBank > data.bankSpace) {
-            return message.channel.send("You don't have that much space in your bank. ");
+        if (parseInt(depAmount) + data.coinsInBank > data.bankSpace) {
+            let bankerrorembed = new MessageEmbed()
+            .setColor("RED")
+            .setDescription(`${x} **${member.user.username}** : You don't have that much space in your bank.`);
+
+            return message.channel.send(bankerrorembed).catch();
+            //return message.channel.send("You don't have that much space in your bank. ");
         }
 
-        data.coinsInBank += parseInt(args[0]);
+        data.coinsInBank += parseInt(depAmount);
 
-        await message.channel.send(`Deposited **${args[0]}** coins.`);
+            let depamountembed = new MessageEmbed()
+            .setColor("BLUE")
+            .setDescription(`${i} **${member.user.username}** : Deposited **${(depAmount).toLocaleString()}** coins.`);
 
-        data.coinsInWallet -= parseInt(args[0]);
+            return message.channel.send(depamountembed).catch();
+      //  await message.channel.send(`Deposited **${(depAmount).toLocaleString()}** coins.`);
+
+        data.coinsInWallet -= parseInt(depAmount);
 
         await data.save();
     }
@@ -65,5 +127,5 @@ module.exports.config = {
     userPerms: [], // User permissions needed to run command. Leave empty if nothing.
     aliases: ['dep'], // Aliases 
     bankSpace: 0, // Amount of bank space to give when command is used.
-    cooldown: 10 // Command Cooldown
+    cooldown: 5 // Command Cooldown
 }
