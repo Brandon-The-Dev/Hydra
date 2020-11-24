1565ecff9da32f943db92e63d43a5e676046224d
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-24 19:20:05 +0000

    🔥 Hydra+ System Update v2.0

--- src/commands/withdraw.js
+++ src/commands/withdraw.js
@@ -1,28 +1,65 @@
+const i = '<:info:688057843558908013>'
+const x = '<:noov:695993429087354991> '
+const tick = '<:bigtick:779736050892931082>'
+const { MessageEmbed } = require('discord.js');  
+
 module.exports.run = async (bot, message, args) => {
     let data = await bot.fetchUser(message.author.id);
-
+   const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
     if (args.join(' ') === 'all') {
-        data.coinsInWallet += data.coinsInBank;
-
-        await message.channel.send(`Withdrawed **${data.coinsInBank}** coins.`);
+                  if (data.coinsInBank === 0) {
+            let bankerrorembed = new MessageEmbed()
+            .setColor("RED")
+            .setDescription(`${x} **${member.user.username}** : Your bank is empty.`);
+            return message.channel.send(bankerrorembed).catch();
+                  }
+        data.coinsInWallet += data.coinsInBank;{
+          
+                let with2embed = new MessageEmbed()
+              .setColor("BLUE")
+              .setDescription(`${i} **${member.user.username}** : Withdrawed **${data.coinsInBank.toLocaleString()}** coins.`);
+              await message.channel.send(with2embed);
+          ////await message.channel.send(`Withdrawed **${data.coinsInBank}** coins.`);
+          
+        }
 
         data.coinsInBank -= data.coinsInBank;
 
         await data.save();
     } else {
-        if (isNaN(args[0])) {
-            return message.channel.send('That\'s not a number.');
+      let withAmount = parseInt(args[0]);
+            if (withAmount === 0) {
+            let bankerrorembed = new MessageEmbed()
+            .setColor("RED")
+            .setDescription(`${x} **${member.user.username}** : Your can't withdraw 0 coins.`);
+            return message.channel.send(bankerrorembed).catch();
+                  }
+        if (isNaN(withAmount)) {
+            let numbererrorembed = new MessageEmbed()
+            .setColor("RED")
+            .setDescription(`${x} **${member.user.username}** : That's not a number.`);
+
+            return message.channel.send(numbererrorembed).catch();
+            //return message.channel.send('That\'s not a number.');
         }
 
-        if (parseInt(args[0]) > data.coinsInBank) {
-            return message.channel.send('You do not have that much coins.');
+        if (parseInt(withAmount) > data.coinsInBank) {
+               let with3embed = new MessageEmbed()
+              .setColor("BLUE")
+              .setDescription(`${i} **${member.user.username}** : You do not have that many coins in your bank.`);
+              return message.channel.send(with3embed);
+            //return message.channel.send('You do not have that much coins.');
         }
 
-        data.coinsInWallet += parseInt(args[0]);
+        data.coinsInWallet += parseInt(withAmount); {
+              let with4embed = new MessageEmbed()
+              .setColor("BLUE")
+              .setDescription(`${i} **${member.user.username}** : Withdrawed **${(withAmount).toLocaleString()}** coins.`);
+              await message.channel.send(with4embed);
+          ///await message.channel.send(`Withdrawed **${args[0]}** coins.`);
+        }
 
-        await message.channel.send(`Withdrawed **${args[0]}** coins.`);
-
-        data.coinsInBank -= parseInt(args[0]);
+        data.coinsInBank -= parseInt(withAmount);
 
         await data.save();
     }
@@ -35,6 +72,6 @@ module.exports.config = {
     botPerms: [], // Bot permissions needed to run command. Leave empty if nothing.
     userPerms: [], // User permissions needed to run command. Leave empty if nothing.
     aliases: ['with'], // Aliases 
-    bankSpace: 0, // Amount of bank space to give when command is used.
+    bankSpace: 3, // Amount of bank space to give when command is used.
     cooldown: 5 // Command Cooldown
 }
