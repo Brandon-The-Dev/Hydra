483b603b9942370f875439eb133bac4d01727277
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-24 19:18:00 +0000

    🔥 Hydra+ System Update v2.0

--- src/commands/use.js
+++ src/commands/use.js
@@ -1,22 +1,49 @@
 const itemss = require('../utils/items');
+const i = '<:info:688057843558908013>'
+const x = '<:noov:695993429087354991> '
+const tick = '<:bigtick:779736050892931082>'
+const { MessageEmbed } = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
+  const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
     let user = await bot.fetchUser(message.author.id);
     if (!args.join(' ')) {
-        return message.channel.send("you can't use nothing lmao");
+      
+              let use1embed = new MessageEmbed()
+              .setColor("RED")
+              .setDescription(`${x} **${member.user.username}** : You forgot to type the items \`id\`.`);
+              return message.channel.send(use1embed);
+        //////return message.channel.send("you can't use nothing lmao");
+      
     }
     const item = itemss.find(x => x.name.toLowerCase() === args.join(' ').toString().toLowerCase());
     if (!item) {
-        return message.channel.send("can't use this item");
+      
+              let use2embed = new MessageEmbed()
+              .setColor("RED")
+              .setDescription(`${x} **${member.user.username}** : You dont have this item make sure you have typed the correct \`id\`.`);
+              return message.channel.send(use2embed);
+        //////return message.channel.send("can't use this item");
+      
     }
     if (!item.canUse) {
-        return message.channel.send(":thinking: You can't use this item");
+
+              let use3embed = new MessageEmbed()
+              .setColor("RED")
+              .setDescription(`${x} **${member.user.username}** : You can't use this item.`);
+              return message.channel.send(use3embed);
+        //////return message.channel.send(":thinking: You can't use this item");
+
     }
     let founditem = user.items.find(x => x.name.toLowerCase() === item.name.toLowerCase());
     let array = [];
     array = user.items.filter(x => x.name !== item.name);
     if (!founditem) {
-        return message.channel.send("you don't have this item");
+              let use3embed = new MessageEmbed()
+              .setColor("RED")
+              .setDescription(`${x} **${member.user.username}** : You dont have this item make sure you have typed the correct \`id\`.`);
+              return message.channel.send(use3embed);
+        //////return message.channel.send("you don't have this item");
     }
     
     if (item.keep == false) {
@@ -44,6 +71,6 @@ module.exports.config = {
     botPerms: [], // Bot permissions needed to run command. Leave empty if nothing.
     userPerms: [], // User permissions needed to run command. Leave empty if nothing.
     aliases: [], // Aliases 
-    bankSpace: 0, // Amount of bank space to give when command is used.
+    bankSpace: 3, // Amount of bank space to give when command is used.
     cooldown: 5 // Command Cooldown
 }
