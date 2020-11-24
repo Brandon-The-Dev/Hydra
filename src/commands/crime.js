a70b5895960aca6965db7bb56dbbdfd659dfc9b0
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-24 18:43:37 +0000

    🔥 Hydra+ System Update v2.0

--- src/commands/crime.js
+++ src/commands/crime.js
@@ -1,19 +1,31 @@
+const { MessageEmbed } = require("discord.js");
+const i = '<:info:688057843558908013>'
+const x = '<:noov:695993429087354991> '
+const tick = '<:bigtick:779736050892931082>'
 module.exports.run = async (bot, message, args) => {
+  
+    const usertag = message.member;
     const another = Math.round(Math.random() * 15);
-    if (another === 4) {
-        return message.channel.send(`**Brnadon-Dev**: lol your POOR !`);
-    }
+
     const random = Math.round(Math.random() * 100);
     const randomMessage = [
-        `You assassinated **Bill Gates** you was payed ${random.toLocaleString()} coins.`,
+      `You assassinated **Bill Gates** you was payed ${random.toLocaleString()} coins.`,
       `You stole from a poor old grannie and she only had ${random.toLocaleString()} coins.`,
       `You raided a drug dealers home and found ${random.toLocaleString()} coins.`,
       `You murdered **Donald Trump** you was payed ${random.toLocaleString()} coins.`,
-      `you almost got shot, but you had **GODMODE** enabled and killed him you was payed ${random.toLocaleString()} coins.`,
+      `You almost got shot, but you had **GODMODE** enabled and killed him you was payed ${random.toLocaleString()} coins.`,
     ];
     const response = randomMessage[Math.floor((Math.random() * randomMessage.length))];
+  /*
     await message.reply(`${response}`)
-    .catch();
+    .catch();*/
+  
+      let begembed = new MessageEmbed()
+    .setColor("GREEN")
+    .setDescription(`${tick} **${usertag.user.username}** : ${response}`);
+
+    await message.channel.send(begembed).catch();
+  
     await bot.giveCoins(message.author.id, random);
 }
 
@@ -27,4 +39,3 @@ module.exports.config = {
     bankSpace: 15, // Amount of bank space to give when command is used.
     cooldown: 5 // Command Cooldown
 }
-
