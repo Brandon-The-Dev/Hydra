c1fd15cac3b9f22e9dba1ae81c6cbb0871b8dfc1
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-24 21:14:16 +0000

    Update dice.js

--- src/commands/dice.js
+++ src/commands/dice.js
@@ -1,5 +1,7 @@
 const { MessageEmbed } = require("discord.js");
-const x = '<:noov:695993429087354991>'
+const i = '<:infomation:779736273639440394>'
+const x = '<:bigx:779736072367505449>'
+const tick = '<:bigtick:779736050892931082>'
 module.exports.run = async (bot, message, args) => {
     const botRoll = Math.floor(Math.random() * 7)+1;
     const userChoice = Math.floor(Math.random() * 7)+1;
