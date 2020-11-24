b9cd07f14e32c54555f6930a955639d07c69a160
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2020-11-24 19:02:50 +0000

    🔥 Hydra+ System Update v2.0

--- src/commands/rob.js
+++ src/commands/rob.js
@@ -1,18 +1,62 @@
+const { MessageEmbed, Message } = require('discord.js');
+const i = '<:info:688057843558908013>'
+const x = '<:noov:695993429087354991> '
+const tick = '<:bigtick:779736050892931082>'
+const s = '<:hydrashild:780113155744595978>'
 module.exports.run = async (bot, message, args) => {
+    const usertag = message.member;
     const user = await bot.fetchUser(message.author.id);
-    if (user.passive == true) return message.channel.send(`You're in passive mode, turn that off to rob others`);
     const member = message.mentions.members.first() || message.guild.members.cache.get(args.join(' ')) || message.guild.members.cache.find(member => member.user.username.toLowerCase() === args.join(' ').toString().toLowerCase());
+  
+  
+    let passivewarn = new MessageEmbed()
+    .setColor("RED")
+    .setDescription(`${x} **${usertag.user.username}** : You have \`PASSIVE\` enabled, your reqired to disable it to use this command.`);
+  
+    if (user.passive == true) return message.channel.send(passivewarn);
+  
+      let roleerrorembed = new MessageEmbed()
+    .setColor("RED")
+    .setDescription(`${x} **${usertag.user.username}** : You can't rob a bot.`);
+    let role = message.mentions.roles.first();
+    if(!role) return message.channel.send(roleerrorembed);
+  
+  
     if (!member) {
-        return message.channel.send("You think you can rob nobody?");
+      
+    let rob1embed = new MessageEmbed()
+    .setColor("RED")
+    .setDescription(`${x} **${usertag.user.username}** : You forgot to tag the person you wanted to rob.`);
+    return message.channel.send(rob1embed);
+    //return message.channel.send("You think you can rob nobody?");
+      
     }
     const devs = ['404205935251292160'];
 
-    if (devs.includes(member.user.id)) return message.channel.send(`You can't rob the bot devs lol.`);
+    if (devs.includes(member.user.id)) {
+      
+    let rob2embed = new MessageEmbed()
+    .setColor("BLUE")
+    .setDescription(`${s} **${usertag.user.username}** : The user you tried to rob has protections`);
+    return message.channel.send(rob2embed);
+      //return message.channel.send(`You can't rob the bot devs lol.`);
+    }
     
     const robbedUser = await bot.fetchUser(member.id);
-    if (robbedUser.passive == true) return message.channel.send(`Leave them alone... they are in passive mode`);
+    if (robbedUser.passive == true) {
+      
+    let rob3embed = new MessageEmbed()
+    .setColor("RED")
+    .setDescription(`${x} **${usertag.user.username}** : The user you tried to rob has passive \`ENABLED\`.`);
+    return message.channel.send(rob3embed);
+      //return message.channel.send(`Leave them alone... they are in passive mode`);
+    }
     if (robbedUser.coinsInWallet < 1000) {
-        return message.channel.send("This user doesn't have much coins, I wouldn't rob them");
+    let rob4embed = new MessageEmbed()
+    .setColor("BLUE")
+    .setDescription(`${s} **${usertag.user.username}** : The user you tried to rob has protections on there balance at the moment as they have a balance smaller then \`1,000\` coins.`);
+    return message.channel.send(rob4embed);
+        //return message.channel.send("This user doesn't have much coins, I wouldn't rob them");
     }
     if (user.items.find(x => x.name == 'luckyclover')) {
         const newInv = user.items.filter(i => i.name != 'luckyclover');
@@ -26,13 +70,23 @@ module.exports.run = async (bot, message, args) => {
     } else {
         const random = Math.floor(Math.random() * 5);
         if (random === 3) {
-            return message.channel.send(`You tried to rob **${member.user.tag}** but got caught👮! Better luck next time.`);
+              let rob5embed = new MessageEmbed()
+              .setColor("BLUE")
+              .setDescription(`${s} **${usertag.user.username}** : You tried to rob **${member.user.tag}** but got caught👮! Better luck next time.`);
+              return message.channel.send(rob5embed);
+            //return message.channel.send(`You tried to rob **${member.user.tag}** but got caught👮! Better luck next time.`);
         }
     }
     let array = robbedUser.items.filter(x => x.name !== 'padlock');
     const padlock = robbedUser.items.find(x => x.name === 'padlock');
     if (padlock) {
-        message.channel.send(`You tried to rob **${member.user.tag}**, but they had a **Padlock**🔒. Try again next time.`);
+      
+              let rob6embed = new MessageEmbed()
+              .setColor("BLUE")
+              .setDescription(`${s} **${usertag.user.username}** : You tried to rob **${member.user.tag}**, but they had a **Padlock**🔒. Try again next time.`);
+              message.channel.send(rob6embed);
+        //message.channel.send(`You tried to rob **${member.user.tag}**, but they had a **Padlock**🔒. Try again next time.`);
+      
         if (padlock.amount === 1) {
             robbedUser.items = array;
             await robbedUser.save();
@@ -40,7 +94,7 @@ module.exports.run = async (bot, message, args) => {
         }
         else {
             array.push({
-                name: 'padlock',
+                name: 'Padlock',
                 amount: padlock.amount - 1,
                 description: padlock.description
             });
@@ -54,7 +108,13 @@ module.exports.run = async (bot, message, args) => {
     robbedUser.coinsInWallet -= randomAmount;
     await user.save();
     await robbedUser.save();
-    message.channel.send(`:moneybag: You stole **${randomAmount.toLocaleString()}** coins from ${member}!`);
+  
+              let rob6embed = new MessageEmbed()
+              .setColor("GREEN")
+              .setDescription(`:moneybag:  **${usertag.user.username}** : You stole **${randomAmount.toLocaleString()}** coins from ${member}!`);
+              message.channel.send(rob6embed);
+  
+    //message.channel.send(`:moneybag: You stole **${randomAmount.toLocaleString()}** coins from ${member}!`);
 
 }
 
@@ -66,6 +126,5 @@ module.exports.config = {
     userPerms: [], // User permissions needed to run command. Leave empty if nothing.
     aliases: [], // Aliases 
     bankSpace: 5, // Amount of bank space to give when command is used.
-    xp: 1,
-    cooldown: 300 // Command Cooldown
+    cooldown: 3 // Command Cooldown
 }
