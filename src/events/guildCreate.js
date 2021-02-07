7ba9beca4bc9fe8af5bf1581200527c8472eb8c8
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2021-02-07 22:09:45 +0000

    Update guildCreate.js

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,23 +1,16 @@
+const { MessageEmbed } = require('discord.js');
 module.exports = async (bot, guild) => {
-    
-    
-    await bot.dbl.postStats(bot.guilds.cache.size);
-
-    
-    bot.on('guildCreate', guild => {
 
   var embed = new MessageEmbed()
         .setColor("#8C00FF")
         .setDescription(`
 \n
 Hello, am Hydra+ thank you for inviting me to your server.
-
 To see the list of commands type [\`h help\`](https://docs.brandondev.xyz/)
-
 **Links** :
 [\`Support Server\`](https://discord.gg/XqezQaK) | [\`Invite Me\`](https://discord.com/oauth2/authorize?client_id=679710920334639115&permissions=8&scope=bot) | [\`Vote For Me\`](https://top.gg/bot/679710920334639115/vote) | [\`Documentation\`](https://docs.brandondev.xyz/)
 `)
 		   guild.systemChannel.send(embed)
-});
-}
 
+ await bot.dbl.postStats(bot.guilds.cache.size);
+}
