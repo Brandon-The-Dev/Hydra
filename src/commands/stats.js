c645b310ef3786e7b31a485ce82404f0095e9207
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2021-02-10 19:09:05 +0000

    Update stats.js

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -2,17 +2,21 @@ const { MessageEmbed } = require("discord.js")
 const i = '<:infomation:779736273639440394>'
 module.exports.run = async (bot, message, args) => {
     const Embed = new MessageEmbed()
-    .setDescription(`${i} Hydra+ is currently deployed in [ **${bot.guilds.cache.size.toLocaleString()}** ]  servers.`)
-    .setColor("BLUE")
+    .setDescription(`${i} Hydra+ Live Stats :`)
+    .addField(`**Servers :**`,` [** [\`${bot.guilds.cache.size.toLocaleString()}\`](https://docs.brandondev.xyz/)** ]`)
+    .addField(`**Users :**`,`[ **[\`${message.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}\`](https://docs.brandondev.xyz/) **] `)
+    .addField(`**Links** :`, "[\`Support Server\`](https://discord.gg/XqezQaK) | [\`Invite Me\`](https://discord.com/oauth2/authorize?client_id=679710920334639115&permissions=8&scope=bot) | [\`Vote For Me\`](https://top.gg/bot/679710920334639115/vote) | [\`Documentation\`](https://docs.brandondev.xyz/) ")
+    .setThumbnail(bot.user.displayAvatarURL({ format: 'png', size: 256, dynamic: true }))
+    .setColor("BLUE") //[\`s\`](https://docs.brandondev.xyz/)
     message.channel.send(Embed);
 }
 module.exports.config = {
-    name: 'servers',
+    name: 'stats',
     description: 'See the bot\'s statisctics',
-    usage: 'h servers',
+    usage: 'h stats',
     botPerms: [],
     userPerms: [],
-    aliases: ['servers'],
+    aliases: ['servers', 'users', 'stat'],
     bankSpace: 1,
     cooldown: 5
 }
