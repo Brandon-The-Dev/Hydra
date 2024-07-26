ccafdebe7665716bcff8d69af7f43d271e67947c
Brandon-The-Dev <73597277+Brandon-The-Dev@users.noreply.github.com>
2024-07-26 20:18:46 +0100

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async bot => {
-    bot.user.setActivity(`${bot.guilds.cache.size.toLocaleString()} Servers | h help`, { type: 'WATCHING' });
+    bot.user.setActivity(`${bot.guilds.cache.size.toLocaleString()} Servers | h help`, { type: 'IDEL' });
     console.log(`${bot.user.tag} is online. ${bot.guilds.cache.size.toLocaleString()} Users amount: ${bot.users.cache.size}`);
     /*await bot.dbl.postStats(bot.guilds.cache.size);*/
 }
